e2330408b8f31b339814823ea31a4e162fc62421
Sano7707 <73274379+Sano7707@users.noreply.github.com>
2023-01-15 22:19:26 +0400

    Update AddUserServlet.java

--- src/main/java/com/example/servlet/AddUserServlet.java
+++ src/main/java/com/example/servlet/AddUserServlet.java
@@ -11,21 +11,35 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Set;
 
-@WebServlet("/users")
-public class GetUsersServlet extends HttpServlet {
+@WebServlet("/add")
+public class AddUserServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
-        Set<User> users = Warehouse.getInstance().getUsers();
-        req.setAttribute("users",users);
-        RequestDispatcher requestDispatcher = req.getRequestDispatcher("jsp/users.jsp");
-       try {
-           requestDispatcher.forward(req, resp);
-       }catch (ServletException | IOException e){
-           e.printStackTrace();
-       }
+        RequestDispatcher requestDispatcher = req.getRequestDispatcher("jsp/add.jsp");
+        try {
+            requestDispatcher.forward(req, resp);
+        } catch (ServletException | IOException e) {
+            e.printStackTrace();
+        }
     }
 
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String firstName = req.getParameter("firstName");
+        String lastName = req.getParameter("lastName");
+        User user = new User(firstName, lastName);
+        Warehouse warehouse = Warehouse.getInstance();
+        warehouse.addUser(user);
+
+        req.setAttribute("userName", firstName);
+        req.setAttribute("lastName", lastName);
+
+        try{
+            resp.sendRedirect("/add");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
+		
